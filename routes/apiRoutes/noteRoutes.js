f1d450f8aa82a58b109c72d48387d152a4409543
Riz <80712058+rizznn@users.noreply.github.com>
2021-09-09 23:05:15 -0400

    added readfile on each route and fixed delete route

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,44 +2,56 @@ const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
 const { findById, createNewNote, validateNote, deleteNote } = require('../../lib/notes');
-const { notes } = require('../../db/db');
+const notes = require('../../db/db');
 
 // To add route
 router.get('/notes', (req, res) => {
-    let results = notes ;
-    console.log(req.query)
-    res.json(results);
+    fs.readFile("./db/db.json", function(err, data) {
+        if (err) throw err;
+    //     var notes = JSON.parse(data);
+    // let results = notes ;
+    // console.log(req.query)
+    // res.json(results);
+    res.json(JSON.parse(data))
+    })
+    
 });
 
-router.get('/notes/:id', (req, res) => {
-    const result = findById(req.params.id, { notes });
-    if (result) {
-      res.json(result);
-    } else {
-      res.send(404);
-    }
-});
+// router.get('/notes/:id', (req, res) => {
+//     const result = findById(req.params.id, { notes });
+//     if (result) {
+//       res.json(result);
+//     } else {
+//       res.send(404);
+//     }
+// });
 
 router.post('/notes', (req, res) => {
+    fs.readFile("./db/db.json", function(err, data) {
+        if (err) throw err;
     // req.body is where our incoming content will be
     // set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
+        req.body.id = notes.length.toString();
 
     // if any data in req.body is incorrect, send 400 error back
-    if (!validateNote(req.body)) {
-        res.status(400).send('The note is not properly formatted.');
-    } else {
-        const note = createNewNote(req.body, notes );
-        res.json(note);
-    }
+        if (!validateNote(req.body)) {
+            res.status(400).send('The note is not properly formatted.');
+        } else {
+            const note = createNewNote(req.body, notes );
+            res.json(note);
+        }
+    })
 });  
 
 // to delete
 router.delete('/notes/:id', (req, res) => {
-    let result = deleteNote(req.params.id, notes );
-    result = result.filter(({ id }) => id !== req.params.id);
-    // fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(result));
-    res.json(result);
+    fs.readFile("./db/db.json", function(err, data) {
+        if (err) throw err;
+        let result = deleteNote(req.params.id, notes );
+        result = result.filter(({ id }) => id !== req.params.id);
+        fs.writeFileSync(path.join(__dirname, '../../db/db.json'), JSON.stringify(result));
+        res.json(result);
+    })
 
 });
 
